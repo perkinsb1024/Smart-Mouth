c6d44152b08818d04ed22e72f37c064cf3f754be
Ben Perkins <benperkins@Bens-MacBook-Pro-2.local>
2025-10-14 21:17:17 -0700

    Cleaned up layout, added word lookup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { WORDS } from './words'
 import { COMMON_WORDS } from './words_common'
 
 export function Tile({ value, color, setValue }) {
+  const [selected, setSelected] = useState(false);
   const isLetter = function(c) {
     if (!c || c.length !== 1) { return false; }
     const code = c.charCodeAt(0);
@@ -19,15 +20,26 @@ export function Tile({ value, color, setValue }) {
     }
   }
   return (
-    <div className={`tile ${color}`} tabIndex="0" onKeyDown={updateLetter}>
-      {value}
-    </div>
+    <>
+      <input type="text" className="tile-hidden-input" id={`tile-hidden-input-${color}`} value={value} onKeyDown={updateLetter} onFocus={() => setSelected(true)} onBlur={() => setSelected(false)} />
+      <div className={`tile ${color} ${selected ? 'selected' : ''}`} onClick={() => document.getElementById(`tile-hidden-input-${color}`).focus()}>
+        {value}
+      </div>
+    </>
   );
 }
 
 export function AnswerWord({ word }) {
+  const [showLookup, setShowLookup] = useState(false);
   return (
-    <div className={`answer-word ${word.common ? 'highlighted' : ''}`} onClick={() => window.open(`https://www.google.com/search?q=define%3A${word.word.toLowerCase()}`).focus()}>{word.word}</div>
+    <div className="answer-word-container" tabIndex="0" onFocus={() => setShowLookup(true)} onBlur={() => setShowLookup(false)} onMouseOver={() => setShowLookup(true)} onMouseOut={() => setShowLookup(false)}>
+      <div className={`word-lookup ${showLookup ? '' : 'hidden'}`} onClick={() => window.open(`https://www.google.com/search?q=define%3A${word.word.toLowerCase()}`).focus()}>
+      <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 4H6C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V14M11 13L20 4M20 4V9M20 4H15" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      </div>
+      <div className={`answer-word ${word.common ? 'highlighted' : ''}`}>{word.word}</div>
+    </div>
   )
 }
 
@@ -66,28 +78,31 @@ export function PlayerList({ players, setPlayers }) {
   )
 }
 
-export function PlayerEditList({ show, players, setPlayers, setShow }) {
+export function Settings({ show, players, setPlayers, setShow, minWordLength, setMinWordLength }) {
   if (!show) { return null; }
   return (
     <>
-      <div className="player-edit-container">
-        <div className="player-edit-list">
-          {players.map((player, i) => (
-            <div key={i}>
-              <input type="text" value={player.name} placeholder={`Player ${i+1}`} onChange={(e) => {
-                players[i].name = e.target.value;
-                setPlayers([...players]);
-              }} />
-              <input type="button" value="X" onClick={() => {
-                if((players[i].name === "" && players[i].score === 0) || confirm(`Are you sure you want to remove player "${player.name || `Player ${i+1}`}"?`)) {
-                  players.splice(i, 1);
+      <div className="settings-container">
+        <div className="settings">
+          <div className="container player-edit-list">
+            {players.map((player, i) => (
+              <div key={i} className="player-row">
+                <input type="text" value={player.name} placeholder={`Player ${i+1}`} onChange={(e) => {
+                  player.name = e.target.value;
                   setPlayers([...players]);
-                }
-              }} />
-            </div>
-          ))}
-
-          <div>
+                }} />
+                <div class="remove-player-button" onClick={() => {
+                  if((player.name === "" && player.score === 0) || confirm(`Are you sure you want to remove player "${player.name || `Player ${i+1}`}"?`)) {
+                    players.splice(i, 1);
+                    setPlayers([...players]);
+                  }
+                }}>
+                  <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path id="Vector" d="M9 9L11.9999 11.9999M11.9999 11.9999L14.9999 14.9999M11.9999 11.9999L9 14.9999M11.9999 11.9999L14.9999 9M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z" stroke="#000000" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                </div>
+              </div>
+            ))}
             <input type="button" value="Add new player" onClick={() => {
               players.push({
                 name: "",
@@ -95,6 +110,25 @@ export function PlayerEditList({ show, players, setPlayers, setShow }) {
               });
               setPlayers([...players]);
             }} />
+          </div>
+
+          <div className="container reset-scores">
+            <input type="button" onClick={() => { 
+                if (confirm("Are you sure you want to reset all scores?")) {
+                  players.forEach(player => {
+                    player.score = 0;
+                  });
+                  setPlayers([...players]);
+                }
+              }} value="Reset Scores" />
+          </div>
+
+          <div className="container min-word-length">
+            <div>Minimum Word Length</div>
+            <div><input type="number" min="1" max="20" value={minWordLength} onFocus={(e) => {e.target.select()}} onChange={e => setMinWordLength(e.target.value)} /></div>
+          </div>
+
+          <div className="container done">
             <input type="button" value="Done" onClick={() => setShow(false)} />
           </div>
         </div>
@@ -103,10 +137,15 @@ export function PlayerEditList({ show, players, setPlayers, setShow }) {
   );
 }
 
-export function AnswerCount({ n }) {
+export function AnswerCount({ n, showAnswers, setShowAnswers }) {
   return (
     <div className="answer-count">
-      There are {n} possible answers
+      <div>
+        There are {n} possible answers
+      </div>
+      <div className="show-hide-answers" onClick={() => { setShowAnswers(!showAnswers); }}>
+        ({showAnswers ? 'hide' : 'show'})
+      </div>
     </div>
   );
 }
@@ -219,38 +258,29 @@ export default function Board() {
     <>
       <title>Smart Mouth</title>
       <div class="container">
+        <div className="settings-button" onClick={() => { setEditPlayers(!editPlayers); }}>
+          <svg height="32px" width="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path class="st0" d="M497.569,215.166l-55.345-13.064c-4.032-13.946-9.495-27.27-16.376-39.732l29.85-48.303   c4.57-7.397,3.452-16.946-2.688-23.086l-31.99-31.99c-6.129-6.129-15.678-7.248-23.076-2.678l-48.292,29.851   c-12.462-6.882-25.785-12.344-39.732-16.377l-13.064-55.368C294.856,5.978,287.306,0,278.63,0h-45.237   c-8.678,0-16.227,5.978-18.227,14.419l-13.064,55.368c-13.946,4.032-27.27,9.484-39.732,16.377l-48.303-29.872   c-7.387-4.549-16.946-3.441-23.086,2.699L58.99,90.97c-6.13,6.14-7.248,15.709-2.689,23.087l29.862,48.313   c-6.882,12.462-12.344,25.786-16.367,39.721l-55.378,13.065C5.978,217.165,0,224.704,0,233.392v45.226   c0,8.678,5.978,16.237,14.419,18.226l55.378,13.065c4.032,13.946,9.485,27.259,16.367,39.71l-29.872,48.324   c-4.549,7.398-3.441,16.957,2.699,23.098l31.979,31.979c6.14,6.14,15.709,7.257,23.087,2.688l48.323-29.872   c12.463,6.882,25.786,12.344,39.722,16.366l13.064,55.366c2,8.463,9.549,14.431,18.227,14.431h45.237   c8.677,0,16.226-5.968,18.226-14.431l13.064-55.366c13.937-4.021,27.259-9.484,39.712-16.366l48.312,29.861   c7.398,4.57,16.947,3.452,23.087-2.688l31.989-31.99c6.13-6.129,7.248-15.688,2.678-23.087l-29.861-48.302   c6.893-12.452,12.345-25.774,16.377-39.721l55.366-13.065c8.463-2.001,14.42-9.539,14.42-18.226v-45.238   C512,224.714,506.032,217.165,497.569,215.166z M256.006,303.103c-26.002,0-47.098-21.097-47.098-47.108   s21.097-47.108,47.098-47.108c26.011,0,47.108,21.097,47.108,47.108S282.017,303.103,256.006,303.103z"/>
+          </svg>
+        </div>
         <div className="tile-container">
           <Tile value={startLetter} color="orange" setValue={setStartLetter} />
           <Tile value={endLetter} color="green" setValue={setEndLetter} />
         </div>
         <div className="controls">
           <button onClick={deal}>Deal</button>
-          <button onClick={() => { setShowAnswers(!showAnswers); }}>{showAnswers ? 'Hide' : 'Show'} Answer List</button>
-          <button onClick={() => { setEditPlayers(!editPlayers); }}>Add or Remove Players</button>
-          <button onClick={() => { 
-            if (confirm("Are you sure you want to reset all scores?")) {
-              players.forEach(player => {
-                player.score = 0;
-              });
-              setPlayers([...players]);
-            }
-          }}>Reset Scores</button>
-          <div className="break" />
-          <div>
-            Minimum Word Length: <input type="number" min="1" max="20" value={minWordLength} onChange={e => setMinWordLength(e.target.value)} />
-          </div>
         </div>
         <div>
           <PlayerList players={players} setPlayers={setPlayers} />
         </div>
         <div>
-          <AnswerCount n={answers.length} />
+          <AnswerCount n={answers.length} showAnswers={showAnswers} setShowAnswers={setShowAnswers} />
         </div>
         <div>
           <WordList show={showAnswers} wordList={answers} />
         </div>
         <div>
-          <PlayerEditList show={editPlayers} players={players} setPlayers={setPlayers} setShow={setEditPlayers} />
+          <Settings show={editPlayers} players={players} setPlayers={setPlayers} setShow={setEditPlayers} minWordLength={minWordLength} setMinWordLength={setMinWordLength} />
         </div>
       </div>
     </>
